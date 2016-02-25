34504e9b6199ac413f1045369127b4ad341767d6
suushie_maniac <suushiemaniac@gmail.com>
2016-02-25 04:27:54 +0100

    BLDAnalyze: Bugfix: Parity method setting

--- src/com/suushiemaniac/cubing/bld/analyze/cube/ThreeBldCube.java
+++ src/com/suushiemaniac/cubing/bld/analyze/cube/ThreeBldCube.java
@@ -336,6 +336,7 @@ public class ThreeBldCube extends TwoBldCube {
 
     public void setCornerParityMethod(CornerParityMethod cornerParityMethod) {
         this.cornerParityMethod = cornerParityMethod;
+        this.parseScramble(this.getScramble());
     }
 
     @Override
