1fb51597dd577171609a82671731cf0fdb514d1f
suushie_maniac <suushiemaniac@gmail.com>
2016-02-25 20:22:05 +0100

    Optim: refined breakInOprimizer result sorting

--- src/com/suushiemaniac/cubing/bld/optim/BreakInOptim.java
+++ src/com/suushiemaniac/cubing/bld/optim/BreakInOptim.java
@@ -6,7 +6,6 @@ import com.suushiemaniac.cubing.bld.model.enumeration.PieceType;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class BreakInOptim {
@@ -17,22 +16,15 @@ public class BreakInOptim {
     }
 
     public List<Algorithm> optimizeBreakInsAfter(char target, PieceType type) {
-        List<Algorithm> algList = new ArrayList<Algorithm>();
-        for (char c = 'A'; c < 'Y'; c++) algList.addAll(this.source.getAlg(type, "" + target + c));
-        Collections.sort(algList, new Comparator<Algorithm>() {
-            @Override
-            public int compare(Algorithm o1, Algorithm o2) {
-                //return Integer.compare(o1.getSubGroup().size(), o2.getSubGroup().size());
-                return new Integer(o1.getSubGroup().size()).compareTo(o2.getSubGroup().size());
-            }
-        });
-        Collections.sort(algList, new Comparator<Algorithm>() {
-            @Override
-            public int compare(Algorithm o1, Algorithm o2) {
-                //return Integer.compare(o1.length(), o2.length());
-                return new Integer(o1.length()).compareTo(o2.length());
-            }
-        });
+        List<Algorithm> algList = new ArrayList<>();
+        for (char c = 'A'; c < 'Y'; c++) {
+            List<Algorithm> sourceList = this.source.getAlg(type, ("" + target) + c);
+            if (sourceList == null) continue;
+            algList.addAll(sourceList);
+        }
+        Collections.sort(algList, (o1, o2) -> o1.getSubGroup().toFormatString().compareTo(o2.getSubGroup().toFormatString()));
+        Collections.sort(algList, (o1, o2) -> Integer.compare(o1.getSubGroup().size(), o2.getSubGroup().size()));
+        Collections.sort(algList, (o1, o2) -> Integer.compare(o1.length(), o2.length()));
         return algList;
     }
 }
