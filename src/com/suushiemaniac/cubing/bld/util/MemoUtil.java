052e7f6ba7031cec56f881af7fa12a21712380b3
suushie_maniac <suushiemaniac@gmail.com>
2016-06-05 16:00:51 +0200

    Add memo LPI permutation

---
+++ src/com/suushiemaniac/cubing/bld/util/MemoUtil.java
@@ -0,0 +1,36 @@
+package com.suushiemaniac.cubing.bld.util;
+
+import com.suushiemaniac.cubing.bld.database.CubeH2;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public abstract class MemoUtil {
+    public static List<String> genMemoTree(String pairs, CubeH2 db, int lpiListSizeLimit) throws SQLException {
+        List<String> treeList = new ArrayList<>(Collections.singletonList(""));
+
+        for (String pair : pairs.split("\\s+?")) {
+            if (pair.length() != 2) continue;
+
+            List<String> words = db.readLpi(pair);
+            words = words.subList(0, Math.min(lpiListSizeLimit, words.size()));
+            List<String> oldMemoStrings = new ArrayList<>(treeList);
+
+            for (String oldMemo : oldMemoStrings) {
+                for (String word : words) {
+                    treeList.add(oldMemo + (oldMemo.length() > 0 ? " // " : "") + word);
+                }
+            }
+
+            treeList.removeAll(oldMemoStrings);
+        }
+
+        return treeList;
+    }
+
+    public static List<String> genMemoTree(String pairs, CubeH2 db) throws SQLException {
+        return genMemoTree(pairs, db, Integer.MAX_VALUE);
+    }
+}
