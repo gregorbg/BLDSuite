cd4fddd42506cf97f66de5e6d314fea8cc8e7b64
Gregor Billing <gregor.billing@freiheit.com>
2019-03-18 17:21:55 +0100

    Open up statistical analysis attributes

--- src/main/kotlin/com/suushiemaniac/cubing/bld/analyze/MassAnalyzer.kt
+++ src/main/kotlin/com/suushiemaniac/cubing/bld/analyze/MassAnalyzer.kt
@@ -18,15 +18,15 @@ class MassAnalyzer(val puzzle: TwistyPuzzle, configTag: String) {
                 .dropLastWhile(String::isEmpty)
     }
 
-    fun <T> analyzeScrambles(scrambles: List<Algorithm>, analysisMapping: BldAnalysis.(PieceType) -> T) = this.analyzer.pieceTypes.associateWith { pt ->
+    fun <T> analyzeScrambles(scrambles: List<Algorithm>, analysisMapping: BldAnalysis.(PieceType) -> T) = this.analyzer.executionPieceTypes.associateWith { pt ->
         scrambles.map { this.analyzer.getAnalysis(it) }.map { it.analysisMapping(pt) }.groupingBy { it }.eachCount()
     }
 
-    private fun <T> analyzeScramblesFlat(scrambles: List<Algorithm>, analysisMapping: BldAnalysis.(PieceType) -> Iterable<T>) = this.analyzer.pieceTypes.associateWith { pt ->
+    fun <T> analyzeScramblesFlat(scrambles: List<Algorithm>, analysisMapping: BldAnalysis.(PieceType) -> Iterable<T>) = this.analyzer.executionPieceTypes.associateWith { pt ->
         scrambles.map { this.analyzer.getAnalysis(it) }.flatMap { it.analysisMapping(pt) }.groupingBy { it }.eachCount()
     }
 
-    private fun <T> analyzeScramblesTotal(scrambles: List<Algorithm>, analysisMapping: BldAnalysis.() -> T) = scrambles.map { this.analyzer.getAnalysis(it) }.map { it.analysisMapping() }.groupingBy { it }.eachCount()
+    fun <T> analyzeScramblesTotal(scrambles: List<Algorithm>, analysisMapping: BldAnalysis.() -> T) = scrambles.map { this.analyzer.getAnalysis(it) }.map { it.analysisMapping() }.groupingBy { it }.eachCount()
 
     fun generateRandom(numCubes: Int) = numCubes.asyncList { puzzle.randomScramble }
 }
